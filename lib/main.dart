96241700b82ef013148f0b50af9b33c305aa8835
aicha05berraf <berrafaicha5@gmail.com>
2025-04-21 19:23:04 +0200

    push 2eme 3la jal errorat t3 selma w sql

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,27 @@ class VolunteerApp extends StatelessWidget {
     );
   }
 
-  createMaterialColor(Color color) {}
-}
+  MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    // ignore: deprecated_member_use
+    final int r = color.red, g = color.green, b = color.blue;
 
-// Your existing createMaterialColor function...
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+
+    // ignore: deprecated_member_use
+    return MaterialColor(color.value, swatch);
+  }
+}
